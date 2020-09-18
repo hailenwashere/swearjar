3f29071ff60ad0fa2edab2ef9bf9b0d45963d89d
hailenwashere <53279829+hailenwashere@users.noreply.github.com>
2020-09-17 17:50:27 -0700

    Update swearjar.py

--- swearjar.py
+++ swearjar.py
@@ -15,9 +15,15 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.find('fuck') != -1 :
-        print(message.content.find('fuck'))
-        print(f'{message.author} said the f word!')
-        await message.channel.send('no swearing or steven will eat u')
+    f = open('swears.txt','r')
+    swears = f.read()
+        # swears = 'fuck\nbitch\nshit' 
+    swears = swears.split('\n') 
+     
+    for line in swears:
+        if message.content.find(line) != -1 :
+            print(message.content.find(line))
+            print(f'{message.author} said the {line[0]} word!')
+            await message.channel.send(f'@{message.author} said the {line[0]} word! no swearing or steven will eat u')
 
 client.run(TOKEN)
